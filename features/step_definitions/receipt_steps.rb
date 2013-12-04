2ca472cf9a72e78e3947b0599df1b3c02ca178fa
robshock <robshock16@gmail.com>
2013-12-04 16:35:34 -0700

    sort by date

--- features/step_definitions/receipt_steps.rb
+++ features/step_definitions/receipt_steps.rb
@@ -13,3 +13,18 @@ When /I follow "(.*)"$/ do |sort_choice|
 		click_on("/receipts/new")
 	end
 end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  #flunk "Unimplemented"
+  aString = page.body.to_s
+  if aString.index(e1)!=nil && aString.index(e2) !=nil
+  if aString.index(e1) < aString.index(e2)
+  else
+  assert false,"jr_fail"
+  end
+  else
+  assert false,"jr_fail"
+  end
+end
