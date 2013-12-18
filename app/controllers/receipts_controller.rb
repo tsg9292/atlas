c1dd634f80588b2160c5853607bfa8e802940622
tsg9292 <tsg9292@gmail.com>
2013-12-18 12:06:53 -0700

    date and cost now switch between ascending and descending order

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -9,18 +9,26 @@ class ReceiptsController < ApplicationController
   	def index
 		sort = params[:sort] #|| session[:sort]
 		case sort
-		when 'date'
-    		ordering,@date_header = {:order => :date}, 'hilite'
-    		if (@receipts = Receipt.order("date DESC"))
-    			@receipts = Receipt.order("date ASC")
-    		elsif (@receipts = Receipt.order("date ASC"))
-    			@receipts = Receipt.order("date DESC")
-    		end
-    		return
-		when 'cost'
-			ordering,@cost_header = {:order => :cost}, 'hilite'
-			@receipts = Receipt.order("cost ASC")
+		when 'dateasc'
+    	ordering,@date_header = {:order => :dateasc}, 'hilite'
+    	@receipts = Receipt.order("date")
+    	return
+
+    when 'datedesc'
+      ordering,@date_header = {:order => :datedesc}, 'hilite'
+      @receipts = Receipt.order("date DESC")
+      return
+
+		when 'costasc'
+			ordering,@cost_header = {:order => :costasc}, 'hilite'
+			@receipts = Receipt.order("cost")
 			return
+
+    when 'costdesc'
+      ordering,@cost_header = {:order => :costdesc}, 'hilite'
+      @receipts = Receipt.order("cost DESC")
+      return
+
     when 'store'
       @receipts = Receipt.where("store = (?)", params[:sto])
       return
