c9d1120fda140c87dd882ac5677811e07a0c7bfa
tsg9292 <tsg9292@gmail.com>
2013-12-19 12:04:54 -0700

    fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -12,6 +11,7 @@ end
 
 module Atlas
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
